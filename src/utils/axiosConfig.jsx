363ebbb2fc783ca7a5d5db2254e2c707c84d4a56
Henry-GC <henry.gon.cev@gmail.com>
2024-09-23 12:11:04 -0500

    change to api

--- src/utils/axiosConfig.jsx
+++ src/utils/axiosConfig.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL:"https://chatutelvt.onrender.com",
+    baseURL:"https://api-chatutelvt.onrender.com",
     headers:{
         "Content-Type": "application/json"
     }
